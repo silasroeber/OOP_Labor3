cd068a50c7369aff5f2dacf14f6ea43324ca8091
Eike Hoffmann <eike.hoffmann@ehoff.org>
2016-11-03 16:27:33 +0100

    eigene implementierung von "date" nutzen

--- Professor.java
+++ Professor.java
@@ -1,14 +1,12 @@
 package labor3;
 
-import java.util.Date;
-
 /**
  * Created by eike on 29.10.16.
  */
 public class Professor extends Person {
 
     private int personalNr;
-    private Date eintrittsDatum;
+    private Datum eintrittsDatum;
     private Fachbereich fachbereich;
 
     public Professor(int nr) {
